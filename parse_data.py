307b0c4d8711ed5c95e436a5dccf145bba2240fd
piotr.blachnio <piotr.blachnio@sigmaconnectivity.se>
2017-04-12 14:53:44 +0200

    before gocoding

--- parse_data.py
+++ parse_data.py
@@ -1,15 +1,26 @@
 import csv
-import simplekml
+import simplekml #http://www.simplekml.com/en/latest/
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+csv_number_column = 0
+csv_date_column = 1
+csv_address_column = 2
+csv_cost_column = 3
+csv_sqm_column = 4
+csv_cost_per_sqm_column = 5
+
 
 inputfile = csv.reader(open('data.csv','r'))
 kml=simplekml.Kml()
 
 for row in inputfile:
-  if(row[4]!=''): #remove empty lines
-    kml.newpoint(name=row[2], coords=[(row[3],row[4])])
-    kml.description = "This is a description"
+  if(row[csv_address_column]!=''): #remove empty lines
+    pnt = kml.newpoint(name=row[csv_address_column])
+    pnt.address = row[csv_address_column]
+    pnt.description = " address: " + row[csv_address_column] + " area: " + row[csv_sqm_column] + "sqm"
+    pnt.coords=[(row[3],row[4])]
+    pnt.address=row[csv_address_column]
 
 kml.save('data.kml')
