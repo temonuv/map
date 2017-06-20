65bafce2bf757d1ead71121c07e35845b4e571b7
piotr.blachnio <piotr.blachnio@sigmaconnectivity.se>
2017-06-20 16:50:11 +0200

    todo remove / when searching by google api

--- README.txt
+++ README.txt
@@ -1,4 +1,6 @@
 1. zapisz aktualny plik z licytacjami komorniczymi do pliku data.csv
 2. odpal sktypt pythona pars_data.py
-3. nowo utworzony plik data.kml zapsz do...
+3. nowo utworzony plik data.kml zapsz na dysk google
+4. wejdź na ...
 www.google.pl/maps -> 3 kreski -> your places -> maps -> licytacje komornicze -> open in my maps
+5. wrzuć plik kml na mapy
--- parse_data.py
+++ parse_data.py
@@ -3,10 +3,32 @@ import simplekml #http://www.simplekml.com/en/latest/
 import sys
 import urllib2
 import json
+ 
+def check():
+        found = False
+        for line in already_added_file:
+            if url_address in line:
+                found = True
+                break
 
+        return found
+
+def point_not_present():
+        already_added_file.write(url_address + '\n')
+        #print(row[csv_address_column])
+        response=urllib2.urlopen(url_prefix+url_address+url_postfix).read()
+        data = json.loads(response)
+        lat=data['results'][0]['geometry']['location']['lat']
+        lng=data['results'][0]['geometry']['location']['lng']
+        #print(lat)
+        #print(lng)
+        pnt.coords=[(lng,lat)]
+        pnt.address=row[csv_address_column]
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+NADPISUJ_PLIKI=1
 
 csv_number_column = 0
 csv_date_column = 1
@@ -22,11 +44,14 @@ response=urllib2.urlopen(url_prefix+url_address+url_postfix).read()
 data = json.loads(response)
 lat=data['results'][0]['geometry']['location']['lat']
 lng=data['results'][0]['geometry']['location']['lng']
-print(lat)
-print(lng)
 
 inputfile = csv.reader(open('data.csv','r'))
+already_added_file = open('already_added.txt','r+w+a')
 kml=simplekml.Kml()
+
+already_in_database_counter=0;
+new_in_database_counter=0;
+
 
 for row in inputfile:
   if(row[csv_address_column]!=''): #remove empty lines
@@ -34,17 +59,18 @@ for row in inputfile:
     pnt.address = row[csv_address_column]
     pnt.description = "cena/m2 " + row[csv_cost_per_sqm_column] + " area: " + row[csv_sqm_column]
     url_address="Warszawa, "+row[csv_address_column]
-    print(url_address)
     url_address=url_address.replace(" ", "+")
-    print(url_address)
-    print(row[csv_address_column])
-    response=urllib2.urlopen(url_prefix+url_address+url_postfix).read()
-    data = json.loads(response)
-    lat=data['results'][0]['geometry']['location']['lat']
-    lng=data['results'][0]['geometry']['location']['lng']
-    print(lat)
-    print(lng)
-    pnt.coords=[(lng,lat)]
-    pnt.address=row[csv_address_column]
+
+    found = check()
+    if check():
+        already_in_database_counter = already_in_database_counter+1
+    else:
+        print(url_address)
+        new_in_database_counter = new_in_database_counter+1
+        point_not_present()
 
 kml.save('data.kml')
+
+print "Already in database " + str(already_in_database_counter)
+print "NEW " + str(new_in_database_counter) 
+already_added_file.close()
